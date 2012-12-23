10537d52e6fa54893f8149f5706c4b8daba32f02
Timothy Achumba <tim@6wunderkinder.com>
2012-12-23 20:59:53 +0000

    Updated the blog post again & added some sharing features and ui updates.

--- site/templates/blogarticle.php
+++ site/templates/blogarticle.php
@@ -4,6 +4,8 @@
     <h1><?php echo html($page->title()) ?></h1>
     <?php echo kirbytext($page->text()) ?>
 
+    <div class="tweet-post">Hey, if you liked this post why not <a href="https://twitter.com/intent/tweet?source=webclient&text=<?php echo ('A new blog post from @iam_timm!')?>%20<?php echo rawurlencode($page->title()); ?>%20<?php echo rawurlencode ($page->url()); ?>" target="blank" title="Tweet this">tweet</a> it!</div>
+
     <a class="post-navigation" href="<?php echo url('scribbles') ?>">Back</a>
 
   </article>
