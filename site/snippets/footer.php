b9f3af10ee04373a3fd4e6594811129678a89c77
Timothy Achumba <tim@6wunderkinder.com>
2012-12-23 21:39:16 +0000

    Updates to the footer.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,10 @@
   <?php echo js('http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js') ?>
   <?php echo js('assets/js/scripts.js') ?>
+</div>
+
+<div class="footer">
+  Designed and developed by Timothy with some help from these contributors.
+</div>
 
 </body>
 
