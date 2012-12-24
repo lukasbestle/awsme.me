2daaaa2a058aaadff8aac77980854365c97085e8
Lukas Bestle <lukas@lu-x.me>
2012-12-24 14:56:20 +0100

    Make anything besides text not selectable

--- site/templates/blogarticle.php
+++ site/templates/blogarticle.php
@@ -4,8 +4,10 @@
 <?php } else { header("X-Title: " . $site->title() . " - " . $page->title()); } ?>
 
   <article class="actual-scribble">
-    <h1><?php echo html($page->title()) ?></h1>
-    <?php echo kirbytext($page->text()) ?>
+  	<div class="selectable">
+    	<h1><?php echo html($page->title()) ?></h1>
+    	<?php echo kirbytext($page->text()) ?>
+  	</div>
 
     <div class="tweet-post">Hey, if you liked this post why not <a href="https://twitter.com/intent/tweet?source=webclient&amp;text=<?php echo rawurlencode("A new blog post from @iam_timm! " . $page->title() . " " . $page->tinyurl()); ?>" target="blank" title="Tweet this">tweet</a> it!</div>
 
