de62df85ce947c72553aa6f4de9339ab555f22e1
Lukas Bestle <lukas@lu-x.me>
2012-12-24 12:28:38 +0100

    Make HTML valid

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,6 @@
 
 <div class="footer">
-  Designed and developed by <a href="http://twitter.com/iam_timm">Timothy ッ</a> with help from some <span>contributors.<div class="contributors">Lukas Bestle<br/>Adam Renklint<br/>Dominik Schmidt</div></span>
+  Designed and developed by <a href="http://twitter.com/iam_timm">Timothy ッ</a> with help from some <span>contributors.<span class="contributors">Lukas Bestle<br/>Adam Renklint<br/>Dominik Schmidt</span></span>
 </div>
 
   <?php echo js('http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js') ?>
