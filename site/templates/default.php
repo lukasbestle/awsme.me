d6eefe80ab19a3095a6c6acdcfeac11bbb245eab
Lukas Bestle <lukas@lu-x.me>
2012-12-24 12:34:49 +0100

    Fix wrong templates

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,18 +4,6 @@
 
   <h2><?php echo html($page->title()) ?></h2>
   <p class="body-text"><?php echo kirbytext($page->text()) ?></p>
-  <ul class="scribbles">
-
-    <?php foreach($page->children()->visible()->flip() as $article): ?>
-
-    <li class="scribble">
-      <article>
-        <span class="scribble-date"><?php echo $article->date('M d') ?></span><a class="scribble-title" href="<?php echo $article->url() ?>"><?php echo html($article->title()) ?></a><span class="scribble-word-count"><?php echo str_word_count($article->text()->value, 0) ?> Words</span>
-      </article>
-    </li>
-
-    <?php endforeach ?>
-  </ul>
 
 </section>
 
