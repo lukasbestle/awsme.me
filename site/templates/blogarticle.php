56556db71932ba4f2eadca69d6c156b1ed99ed41
Lukas Bestle <lukas@lu-x.me>
2012-12-25 19:14:42 +0100

    Add Safari Reader support

--- site/templates/blogarticle.php
+++ site/templates/blogarticle.php
@@ -8,12 +8,9 @@
     	<h1><?php echo html($page->title()) ?></h1>
     	<?php echo kirbytext($page->text()) ?>
   	</div>
-
-    <div class="tweet-post">Hey, if you liked this post why not <a href="https://twitter.com/intent/tweet?source=webclient&amp;text=<?php echo rawurlencode("A new blog post from @iam_timm! " . $page->title() . " " . $page->tinyurl()); ?>" target="blank" title="Tweet this">tweet</a> it!</div>
-
-    <a class="post-navigation" href="<?php echo url() ?>">Back</a>
-
   </article>
+  <div class="tweet-post">Hey, if you liked this post why not <a href="https://twitter.com/intent/tweet?source=webclient&amp;text=<?php echo rawurlencode("A new blog post from @iam_timm! " . $page->title() . " " . $page->tinyurl()); ?>" target="blank" title="Tweet this">tweet</a> it!</div>
+  <a class="post-navigation" href="<?php echo url() ?>">Back</a>
 
 <?php if(!r::is_ajax()) { ?>
 </section>
