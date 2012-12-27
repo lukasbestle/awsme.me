5fce5ea96d5e3d3a3830bf5895f94c18f3d155ca
Timothy Achumba <timothy@achumba.com>
2012-12-27 11:01:30 +0000

    Updated the RSS feed copy and styling

--- site/templates/blogarticle.php
+++ site/templates/blogarticle.php
@@ -8,7 +8,7 @@
     	<?php echo kirbytext($page->text()) ?>
   	</div>
 
-    <div class="tweet-post">Liked what you just read? Why not <a href="https://twitter.com/intent/tweet?source=webclient&amp;text=<?php echo rawurlencode("I just read a blog post: " . $page->title() . " (via @iam_timm) " . $page->tinyurl()); ?>" target="blank" title="Tweet this">tweet</a> about it or <a href="http://feeds.feedburner.com/awsme" title="Subscribe to my feed" rel="alternate" type="application/rss+xml">subscribe</a> to get updates on new posts!</div>
+    <div class="tweet-post">Enjoy this post? Why not <a href="https://twitter.com/intent/tweet?source=webclient&amp;text=<?php echo rawurlencode("I just read a blog post: " . $page->title() . " (via @iam_timm) " . $page->tinyurl()); ?>" target="blank" title="Tweet this">tweet</a> about it or <a href="http://feeds.feedburner.com/awsme" title="Subscribe to my feed" rel="alternate" type="application/rss+xml">subscribe</a> to get notified when I post something new!</div>
 
     <a class="post-navigation button" href="<?php echo url() ?>">Back</a>
 
